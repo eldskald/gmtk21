ce993da62a305836b7e08cb73cf915099b08a717
Ayrton <makkun1232@gmail.com>
2021-06-13 12:31:13 -0300

    FIX Level Holder

--- source/common/levels_holder.gd
+++ source/common/levels_holder.gd
@@ -3,11 +3,11 @@ extends Node
 export(Array, PackedScene) var levels
 export(PackedScene) var credits
 
-onready var i : int = 0
+onready var i : int = -1
 
 func load_level():
 	i += 1
 	if i < levels.size():
-		get_tree().change_scene_to(levels[0])
+		get_tree().change_scene_to(levels[i])
 	else:
 		get_tree().change_scene_to(credits)
