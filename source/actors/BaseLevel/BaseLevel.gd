0b6953292bc383ee2b192781e36190cb8b4e99dc
Ayrton <makkun1232@gmail.com>
2021-06-13 06:52:32 -0300

    ADD Wind Sound

--- source/actors/BaseLevel/BaseLevel.gd
+++ source/actors/BaseLevel/BaseLevel.gd
@@ -6,6 +6,7 @@ onready var player_2 = get_tree().get_nodes_in_group("player2")[0]
 
 func _on_FinishLine_body_entered(body):
 	if body.is_in_group("player1") or body.is_in_group("player2"):
+		Bgm.get_node("WinSound").play()
 		player_1.next_state = player_1.CUTSCENE
 		player_2.next_state = player_2.CUTSCENE
 		$FinishLine/Hole/AnimationPlayer.play("ripple")
