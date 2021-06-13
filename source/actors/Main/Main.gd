b110c9d39471dc68242fdb7d45563bc0d2a5fa07
Ayrton <makkun1232@gmail.com>
2021-06-13 03:50:31 -0300

    ADD Title Screen

--- source/actors/Main/Main.gd
+++ source/actors/Main/Main.gd
@@ -1,11 +1,14 @@
 extends Node
 
-var levels = []
+onready var title_label = $MarginContainer/CenterContainer/VBoxContainer/Label
 
-enum {TITLE, GAME}
-
-var state = TITLE
-
+func _ready():
+	$MarginContainer/CenterContainer/VBoxContainer/Label.modulate.a = 0
+	$IntroTimer.start()
+	yield($IntroTimer,"timeout")
+	$AnimationPlayer.play("FadeIN")
+	yield($AnimationPlayer,"animation_finished")
+	$TextTimer.start(1)
 
 func _on_TextTimer_timeout():
 	if $MarginContainer/CenterContainer/VBoxContainer/VBoxContainer/Text.visible == true:
@@ -16,3 +19,7 @@ func _on_TextTimer_timeout():
 		$MarginContainer/CenterContainer/VBoxContainer/VBoxContainer/HSeparator2.hide()
 		$MarginContainer/CenterContainer/VBoxContainer/VBoxContainer/Text.show()
 		$TextTimer.start(0.4)
+
+func _input(event):
+	if (event is InputEventJoypadButton or event is InputEventKey) and !event.is_echo():
+		print("Ola")
